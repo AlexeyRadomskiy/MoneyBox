dd896cbcec9578f361d22d3ae9dbb96a7dce1fb5
Alexey <alexeyradomskiy98@gmail.com>
2022-03-30 21:42:28 +0300

    add structure

---
+++ MoneyBox/Models/GoalModel.swift
@@ -0,0 +1,15 @@
+//
+//  GoalModel.swift
+//  MoneyBox
+//
+//  Created by Alexey on 30.03.2022.
+//
+
+import UIKit
+
+struct GoalModel {
+    var name: String
+    var photo: UIImage
+    var price: Int
+    
+}
