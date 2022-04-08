9110c63cb8056828b5657a57f47a224bc9b4116e
sayabeck <sayabeck@mail.ru>
2022-04-08 21:38:39 +0600

    add Developers screen

---
+++ MoneyBox/Models/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MoneyBox
+//
+//  Created by mac mini on 4/8/22.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var telegramLabel: UILabel!
+    
+}
---
+++ MoneyBox/Models/Developer.swift
@@ -0,0 +1,17 @@
+//
+//  Developer.swift
+//  MoneyBox
+//
+//  Created by mac mini on 4/8/22.
+//
+
+import Foundation
+
+struct Developer {
+    
+    let name: String
+    let city: String
+    let telegram: String
+    let photo: String
+    
+}
