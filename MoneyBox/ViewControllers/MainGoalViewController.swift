7132022ea86f1883ed7c5fb80799d29684c351eb
Alexey <alexeyradomskiy98@gmail.com>
2022-04-16 23:24:23 +0300

    Create NewGoalTableViewController

--- MoneyBox/ViewControllers/MainGoalViewController.swift
+++ MoneyBox/ViewControllers/MainGoalViewController.swift
@@ -2,33 +2,50 @@
 //  MainGoalViewController.swift
 //  MoneyBox
 //
-//  Created by Alexey on 05.04.2022.
+//  Created by Alexey on 16.04.2022.
 //
 
 import UIKit
 
 class MainGoalViewController: UIViewController {
     
-    @IBOutlet var photoLabel: UIImageView!
-    @IBOutlet var goalNameLabel: UILabel!
-    @IBOutlet var weeksLeftLabel: UILabel!
-    @IBOutlet var progressLabel: UILabel!
-    
-    
-    var goal: Goal!
+    var goal = Goal(name: "", photo: nil, price: "", savings: "", income: "", isFavourite: false)
 
+    @IBOutlet weak var photoImage: UIImageView!
+    @IBOutlet weak var isFavouriteButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var weeksLeftLabel: UILabel!
+    @IBOutlet weak var progressLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Моя цель"
-        photoLabel.image = UIImage(named: goal.photo)
-        goalNameLabel.text = "Цель: \(goal.goalName)"
-        weeksLeftLabel.text = "Осталось копить еще \((goal.price - goal.savings) / (goal.income)) недель"
-        progressLabel.text = "Накоплено \(goal.savings) из \(goal.price) рублей"
+        updateUI()
     }
     
-    @IBAction func deductionMoneyButton() {
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        super.prepare(for: segue, sender: sender)
+//        guard segue.identifier == "mainGoal" else { return }
+//        let indexPath = tableView.indexPathForSelectedRow!
+//        let goal = goals[indexPath.row]
+//        let navigationVC = segue.destination as! UINavigationController
+//        let newGoalTableVC = navigationVC.topViewController as! NewGoalTableViewController
+//        newGoalTableVC.goal = goal
+//        newGoalTableVC.title = "Редактирование"
+//    }
+    
+    @IBAction func minusButton(_ sender: UIButton) {
     }
     
-    @IBAction func addingMoneyButton() {
+    @IBAction func plusButton(_ sender: UIButton) {
+    }
+}
+
+// MARK: - Private methods
+extension MainGoalViewController {
+    private func updateUI() {
+        photoImage.image = goal.photo
+        nameLabel.text = goal.name
+        weeksLeftLabel.text = "Осталось \((Int(goal.price)! - Int(goal.savings)!) / Int(goal.income)!) недель"
+        progressLabel.text = "Накоплено \(goal.income) ₽ из \(goal.price) ₽"
     }
 }
