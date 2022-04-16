216702504f779232c1d69348e33b8357b37152fc
Alexey <alexeyradomskiy98@gmail.com>
2022-04-16 23:24:00 +0300

    Create GoalTableViewCell

---
+++ MoneyBox/ViewControllers/GoalTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  GoalTableViewCell.swift
+//  MoneyBox
+//
+//  Created by Alexey on 16.04.2022.
+//
+
+import UIKit
+
+class GoalTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var goalImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var weeksLeftLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        goalImage.layer.cornerRadius = 24
+        nameLabel.font = UIFont.systemFont(ofSize: 17)
+        progressLabel.font = UIFont.boldSystemFont(ofSize: 15)
+        weeksLeftLabel.font = UIFont.preferredFont(forTextStyle: .footnote)
+    }
+    
+    func set(goal: Goal) {
+        self.goalImage.image = goal.photo
+        self.nameLabel.text = goal.name
+        self.progressLabel.text = "Накоплено \(goal.income) ₽ из \(goal.price) ₽"
+        self.weeksLeftLabel.text = "Осталось \((Int(goal.price)! - Int(goal.savings)!) / Int(goal.income)!) недель"
+    }
+}
