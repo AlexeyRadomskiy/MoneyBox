189c735212be045e3053bcdc89d09032ea610b0b
sayabeck <sayabeck@mail.ru>
2022-04-18 20:40:07 +0600

    add DevopsVC

---
+++ MoneyBox/Models/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MoneyBox
+//
+//  Created by mac mini on 4/18/22.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var telegramLabel: UILabel!
+    
+}
---
+++ MoneyBox/Models/Developer.swift
@@ -0,0 +1,17 @@
+//
+//  Developer.swift
+//  MoneyBox
+//
+//  Created by mac mini on 4/18/22.
+//
+
+import Foundation
+
+struct Developer {
+    
+    let name: String
+    let city: String
+    let telegram: String
+    let photo: String
+    
+}
