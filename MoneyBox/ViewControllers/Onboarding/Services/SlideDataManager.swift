147f54c11e3cf370e86c08ea3e4a1997583bd241
Alexey <alexeyradomskiy98@gmail.com>
2022-04-19 21:19:51 +0300

    Update text mistake

--- MoneyBox/ViewControllers/Onboarding/Services/SlideDataManager.swift
+++ MoneyBox/ViewControllers/Onboarding/Services/SlideDataManager.swift
@@ -12,6 +12,6 @@ struct SlideDataManager {
     let data = [
         OnboardingSlide(image: #imageLiteral(resourceName: "firstSlide"), title: "Хотите научить вашего ребенка копить?"),
         OnboardingSlide(image: #imageLiteral(resourceName: "secondSlide"), title: "Мы покажем ему как задать цель и терпеливо идти к ней"),
-        OnboardingSlide(image: #imageLiteral(resourceName: "thirdSlide"), title: "С нами он накопить на мечту прямо в приложении!")
+        OnboardingSlide(image: #imageLiteral(resourceName: "thirdSlide"), title: "С нами он накопит на мечту прямо в приложении!")
     ]
 }
